c2a6c4db87ec5899147f12a859cc9d12928fdc18
Ben Wilcox <reccanti@gmail.com>
2016-11-05 17:53:28 -0400

    Configure database file for heroku
    Adds a check for an environment variable called
    "DATABASE_URL" which heroku uses to set up its
    postgresql database. If it parses the url, it
    will grab the required components and put them
    in the postgresql config section. Otherwise, it
    uses the variables specified in the .env file
    so it can be tested locally

--- config/database.php
+++ config/database.php
@@ -1,5 +1,15 @@
 <?php
 
+$url = parse_url(getenv("DATABASE_URL"));
+
+if ($url && $url["path"]) {
+    var_dump($url);
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+}
+
 return [
 
     /*
@@ -68,11 +78,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', $host),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
